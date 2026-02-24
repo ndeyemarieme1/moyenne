0d29e5bd6d0e03c6fe049db120eba12ec9576d4c
ndeyemarieme1 <ndeyemariemem21@gmail.com>
2026-02-24 12:02:29 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.header("application moyenne")
+
+x = st.number_input("note x ")
+y = st.number_input("note  y")
+
+moy = (x+y)/2
+
+if st.button("OK"):
+    st.info(moy)
